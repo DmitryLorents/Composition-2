b3fc3f7fb7f64530d3aed43b5af7e2315a25cc3c
DmitryLorents <zmey.task@gmail.com>
2023-04-26 10:55:54 +0600

    Initial Commit

---
+++ Composition-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Composition-2
+//
+//  Created by Дмитрий Лоренц on 26.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
